63fedb4d11938952b6b388afdccd77f2c9ac1926
seoulziggs <seoulziggs@naver.com>
2025-01-03 23:43:45 +0900

    Add Annotations

--- Operations.cs
+++ Operations.cs
@@ -1,10 +1,12 @@
 public class Operation
 {
+    // 덧셈 함수
     public int add(int a, int b)
     {
         return a + b;
     }
 
+    // 곱셈 함수
     public int subtract(int a, int b)
     {
         return a - b;
